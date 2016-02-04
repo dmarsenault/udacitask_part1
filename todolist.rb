08a6b134299e9f5ff20c6784dfc082ec37fe88c3
David Arsenault <dmarsenault@gmail.com>
2016-02-04 14:47:22 -0700

    Complete list, fix bugs

--- todolist.rb
+++ todolist.rb
@@ -6,38 +6,44 @@ class TodoList
       @items = Array.new
     end
 
+    # Change the name of the title
     def update_title(new_title)
       @title = new_title
     end
 
+    # Add a new item to the list
     def add_item(new_item)
       item = Item.new(new_item)
       @items.push(item)
     end
 
+    # Remove an item from the list
     def delete_item(item_id)
       @items.delete_at(item_id)
     end
 
+    # Used to divide title from items on list
     def divider
-      print "~" * 40
+      print "~" * 65
     end
 
-    def print_description
-      @items.each do |description|
-        description.print_items
-      end
-    end
-
+    # Uses toggle_status to mark an item in list as complete
     def toggle_completion(item_id)
       @items.at(item_id).toggle_status
     end
 
+    # Outputs the total number of tasks on the list.
+    def total_list
+      @items.length
+    end
+
+    # Prints list
     def print_list
       puts " "
       puts @title
       puts divider
-      puts print_description
+      @items.each { |description| description.print_items }
+      puts "Total tasks: #{total_list}"
     end
 end
 
@@ -50,12 +56,13 @@ class Item
       @completion_status = "( )"
     end
 
+    # Changes completion from ( ) to (X)
     def toggle_status
       @completion_status = "(X)"
     end
 
+    # Allows list to print both completion status and item description
     def print_items
-      puts "#{@completion_status} ----- #{@description}"
+      puts "#{@completion_status} ~~~~ #{@description}"
     end
-
 end
